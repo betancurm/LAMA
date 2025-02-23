3fa396de59c86ff1fdefed7fc6db398ab03eb872
Juan Manuel Betancur <juanbetancur142474@correo.itm.edu.co>
2025-02-23 12:06:06 -0500

    Header construido

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  
+  content: ['./src/**/*.{astro,html,md,mdx,svelte,vue,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
